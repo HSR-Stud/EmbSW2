f21347e4e8b3526192e3a646c8e7269fd1423673
n04hk <noah.kaelin@bluewin.ch>
2020-03-01 21:16:17 +0100

    Kapitel 'Such-Algorithmen' aktualisiert

--- code/BinaereSuche.cpp
+++ code/BinaereSuche.cpp
@@ -1,5 +1,5 @@
-left = 0; 				// Index des am weitesten links liegenden Kandidaten
-right = listLength-1; 	// Index des am weitesten rechts liegenden Kandidaten
+left = 0;               // Index des am weitesten links liegenden Kandidaten
+right = listLength-1;   // Index des am weitesten rechts liegenden Kandidaten
 while (left <= right)
 {
   pos = (left + right) / 2; // pos auf die mittlere Position setzen
--- code/NaiverAlgorithmus.cpp
+++ code/NaiverAlgorithmus.cpp
@@ -1,17 +1,17 @@
 int naivePSearch(const char* text, const char* pattern)
 {
-	int i;
-	int j;
-  
-	for(i=0; text[i]; ++i)
-	{
-		for(j=0; pattern[j]; ++j)
-		{
-			if(pattern[j] != text[i+j])
-				break;
-		}
-		if(pattern[j] == 0)  // pattern found
-			return i;
-	}
-	return -1  // pattern not found
+  int i;
+  int j;
+
+  for(i=0; text[i]; ++i)
+  {
+    for(j=0; pattern[j]; ++j)
+    {
+      if(pattern[j] != text[i+j])
+        break;
+    }
+    if(pattern[j] == 0)  // pattern found
+      return i;
+  }
+  return -1  // pattern not found
 }
