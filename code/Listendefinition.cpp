912a4470147f1216ecd71bb4423e360e9545fcc7
n04hk <noah.kaelin@bluewin.ch>
2020-08-05 17:39:49 +0200

    Fehler korrigiert

--- code/Listendefinition.cpp
+++ code/Listendefinition.cpp
@@ -2,7 +2,7 @@ struct Field
 {
   Key id; 		// z.B. int
   Content cont;         // Typ, der den ganzen Inhalt umfasst
-}
+};
 
 Field list[listLength];
 Key k; 			// der Wert, der gefunden werden soll
